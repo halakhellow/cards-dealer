4d4a6feb36a17605dae25a3517dce4a0c100d33c
Hala Alkhellow <halakhellow1996@gmail.com>
2020-09-28 21:30:39 +0300

    add getCard method

--- src/Deck.js
+++ src/Deck.js
@@ -4,8 +4,10 @@ import Card from "./Card";
 class Deck extends Component {
   constructor(props) {
     super(props);
-    this.state = { deck: null };
+    this.state = { deck: null, drawnCards: [] };
+    this.getCard = this.getCard.bind(this);
   }
+
   componentDidMount() {
     fetch("https://deckofcardsapi.com/api/deck/new/shuffle")
       .then((res) => res.json())
@@ -13,10 +15,36 @@ class Deck extends Component {
         this.setState({ deck: data });
       });
   }
+
+  getCard() {
+    fetch(
+      `https://deckofcardsapi.com/api/deck/${this.state.deck.deck_id}/draw/`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data.success) alert("No more cards");
+        else {
+          let card = data.cards[0];
+          console.log(data);
+          this.setState((st) => ({
+            drawnCards: [
+              ...st.drawnCards,
+              {
+                id: card.code,
+                image: card.image,
+                alt: `${card.value}-${card.suit}`,
+              },
+            ],
+          }));
+        }
+      });
+  }
+
   render() {
     return (
       <div>
         <h1>Cards Dealer</h1>
+        <button onClick={this.getCard}>Get a card !</button>
       </div>
     );
   }
